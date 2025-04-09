9ac71067759ea16871566e5f5cf2261f1822dfb0
almostJohn <garcia.johngale@gmail.com>
2025-04-09 12:24:09 +0800

    refactor: restructure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,8 @@ import type { Metadata } from "next";
 import { inter } from "~/lib/fonts";
 import { cn } from "~/lib/utils";
 import { siteConfig } from "~/config/site";
-import { Navbar } from "~/components/navigation/navbar";
-import { Footer } from "~/components/footer";
+import { SiteHeader } from "~/components/site-header";
+import { SiteFooter } from "~/components/site-footer";
 import "~/styles/globals.css";
 
 export const metadata: Metadata = {
@@ -30,10 +30,10 @@ export default function RootLayout({ children }: { readonly children: React.Reac
 	return (
 		<html lang="en" suppressHydrationWarning>
 			<body className={cn("bg-neutral-50 text-neutral-950 antialiased", inter.className)}>
-				<main className="min-h-screen flex flex-col">
-					<Navbar />
+				<main className="min-h-screen">
+					<SiteHeader />
 					{children}
-					<Footer />
+					<SiteFooter />
 				</main>
 			</body>
 		</html>
